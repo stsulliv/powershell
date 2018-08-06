7dbab1d8b0c3f0decabb4a0d939c63bd71d78fd0
Stephen Sullivan <ssullivan@ugliscripts.com>
2018-08-06 11:34:44 -0400

    Update connect-mso.ps1
    I added some comments and CLI feedback on stratus.  I also updated the sample scripts you can run from the console.

--- office-365-logon/connect-mso.ps1
+++ office-365-logon/connect-mso.ps1
@@ -1,4 +1,9 @@
-# you can include the UPN as a parameter or you will be prompted/
+# This script prompts for credentials and stores them encrypted in the script directory.
+# only the user account that created the stored credential can access it
+#
+# .\connectoToMso.ps1 "first.last.@example.com"
+
+# Checks for UPN passed to script through CLI
 [CmdletBinding()]
 Param(
 
@@ -6,9 +11,10 @@ Param(
         [string]$mso_account
 )
 
+# Always include Path instead of .\ for extensibility
 $PathRoot = $PSScriptRoot
 
-# below stores credentials and loads stored credentials
+# below stores credentials or loads stored credentials
 if (!([System.IO.File]::Exists("$PathRoot\$mso_account.pwd"))) {
     
     $Credential = Get-Credential -Username $mso_account -Message "Enter a user name and password"
@@ -25,35 +31,71 @@ else {
 # wait until you need to MSOnline module
 import-module MSOnline
 
-# Connect to Exchange Online
+# create Exchange Online Session
 $ExchOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
 
-if ($null -ne $ExoSession) {
+if ($null -ne $ExchOnlineSession) {
+    
+    try {
 
-    Import-PSSession $ExoSession
+        Import-PSSession $ExchOnlineSession
+
+    } catch {
+        
+        'STOPPING...Could not connect to Exchange Online'
+        Stop
+    }
+
+    'SUCCESS...Connected to Exchange Online'
 }
 
-# Connect to EOP
+# create Exchange Online Protection (EOP) session
 $EopSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.compliance.protection.outlook.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
 
 if ($null -ne $EopSession) {
+    
+    try {
 
-    Import-PSSession $EopSession -AllowClobber
+        Import-PSSession $EopSession -AllowClobber
+    
+    } catch {
+        
+        'STOPPING...Could not connect to Exchange Online Protection (EOP)'
+        Stop
+    }
+
+    'SUCCESS...Connected to Exchange Online Protection (EOP)'
 } 
 
-# Connect to MS Online
-Connect-MsolService -Credential $Credential
+# Connect to Microsoft Online Session (MSO)
+try {
 
-# this leaves an open connection to Exchange Online, EOP and Azure AD
+    Connect-MsolService -Credential $Credential
 
-#Load "System.Web" assembly in PowerShell console 
-#[Reflection.Assembly]::LoadWithPartialName("System.Web")
+    
+    } catch {
+        
+        'STOPPING...Could not connect to Microsoft Online Service (MSO)'
+        Stop
+    }
 
-# close the Exchange Online and EOP sessions
-# Remove-PSSession $ExchOnlineSession
-# Remove-PSSession $EopSession
+'SUCCESS...Connected to Microsoft Online Service (MSO)'
 
-# if you need to close orphaned PS-sessions
+'Before closing window run Get-PSSession | Remove-PSSession to close Exo and Eop sessions'
+
+#################### the resulting prompt is connected to Exchange Online, EOP and Azure AD ########################
+
+# Before closing window run
 # Get-PSSession | Remove-PSSession
 
-# MsolService connection closes when you close powershell
+######################################### Sample Scripts to copu-and-paste #########################################
+
+# Get-MailboxPermission -Identity "first.last@example.com"
+# Get-MailboxPermission -Identity "first.last@example.com" | Where-Object {($_.IsInherited -ne "True") -and ($_.User -notlike "*SELF*")}
+
+# Get-InboxRule -Mailbox "first.last@example.com"
+# Get-InboxRule -Mailbox "first.last@example.com" | Where-Object {(($_.Enabled -eq $true) -and (($_.ForwardTo -ne $null) -or ($_.ForwardAsAttachmentTo -ne $null) -or ($_.RedirectTo -ne $null) -or ($_.SendTextMessageNotificationTo -ne $null)))}
+
+# Search-UnifiedAuditLog -StartDate (Get-Date).AddDays(-7).ToString('MM/dd/yyyy') -EndDate (Get-Date).ToString('MM/dd/yyyy') -UserIds "first.last@example.com"
+
+# Search-UnifiedAuditLog -StartDate 08/01/2018 -EndDate 08/03/2018
